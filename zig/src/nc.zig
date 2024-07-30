56dda7cde4df24674aed5d246f59b232628f6768
Wison Ye <wisonye@gmail.com>
2024-07-30 18:12:46 +1200

    [ master ] - zig version, 'nc.zig' dislabes 'zig fmt'.

--- zig/src/nc.zig
+++ zig/src/nc.zig
@@ -1,22 +1,24 @@
+// zig fmt: off
+//
 pub const WINDOW = anyopaque;
 pub const chtype = u32;
 
 //
 // Locale related
 //
-pub const LC_CTYPE: c_int = 0;
-pub const LC_NUMERIC: c_int = 1;
-pub const LC_TIME: c_int = 2;
-pub const LC_COLLATE: c_int = 3;
-pub const LC_MONETARY: c_int = 4;
-pub const LC_MESSAGES: c_int = 5;
-pub const LC_ALL: c_int = 6;
-pub const LC_PAPER: c_int = 7;
-pub const LC_NAME: c_int = 8;
-pub const LC_ADDRESS: c_int = 9;
-pub const LC_TELEPHONE: c_int = 10;
-pub const LC_MEASUREMENT: c_int = 11;
-pub const LC_IDENTIFICATION: c_int = 12;
+pub const LC_CTYPE: c_int               = 0;
+pub const LC_NUMERIC: c_int             = 1;
+pub const LC_TIME: c_int                = 2;
+pub const LC_COLLATE: c_int             = 3;
+pub const LC_MONETARY: c_int            = 4;
+pub const LC_MESSAGES: c_int            = 5;
+pub const LC_ALL: c_int                 = 6;
+pub const LC_PAPER: c_int               = 7;
+pub const LC_NAME: c_int                = 8;
+pub const LC_ADDRESS: c_int             = 9;
+pub const LC_TELEPHONE: c_int           = 10;
+pub const LC_MEASUREMENT: c_int         = 11;
+pub const LC_IDENTIFICATION: c_int      = 12;
 pub extern fn setlocale(
     category: c_int,
     locale: ?[*]const u8,
@@ -46,11 +48,7 @@ pub extern fn doupdate() c_int;
 //
 // Print in window related
 //
-pub extern fn wprintw(
-    win: ?*WINDOW,
-    fmt: ?[*]const u8,
-    ...,
-) c_int;
+pub extern fn wprintw( win: ?*WINDOW, fmt: ?[*]const u8, ...) c_int;
 
 pub extern fn mvwprintw(
     win: ?*WINDOW,
@@ -65,20 +63,14 @@ pub extern fn mvwprintw(
 // ----------------------------------------------------------------------------
 
 // Cursor option
-pub const CURSOR_INVISIBLE: c_int = 0;
-pub const CURSOR_NORMAL: c_int = 1;
-pub const CURSOR_VERY_VISIBLE: c_int = 2;
+pub const CURSOR_INVISIBLE: c_int       = 0;
+pub const CURSOR_NORMAL: c_int          = 1;
+pub const CURSOR_VERY_VISIBLE: c_int    = 2;
 
 extern fn curs_set(c_int) c_int;
-pub fn hide_cursor() c_int {
-    return curs_set(CURSOR_INVISIBLE);
-}
-pub fn show_cursor() c_int {
-    return curs_set(CURSOR_NORMAL);
-}
-pub fn show_cursor_2() c_int {
-    return curs_set(CURSOR_VERY_VISIBLE);
-}
+pub fn hide_cursor() c_int { return curs_set(CURSOR_INVISIBLE); }
+pub fn show_cursor() c_int { return curs_set(CURSOR_NORMAL); }
+pub fn show_cursor_2() c_int { return curs_set(CURSOR_VERY_VISIBLE); }
 
 //
 // Get x,y related
@@ -116,56 +108,56 @@ pub fn set_cursor_position(win: ?*WINDOW, pos: *CursorPosition) c_int {
 //
 // Keys
 //
-pub const KEY_DEL: c_int = 0x7F; // 127, ^?
-pub const KEY_ESCAPE: c_int = 0x1B; // 27, ^[
-pub const KEY_TICK: c_int = 0x69; // 96, `
-pub const KEY_TILDE: c_int = 0x7E; // 126, ~
+pub const KEY_DEL: c_int                = 0x7F; // 127, ^?
+pub const KEY_ESCAPE: c_int             = 0x1B; // 27,  ^[
+pub const KEY_TICK: c_int               = 0x69; // 96,  `
+pub const KEY_TILDE: c_int              = 0x7E; // 126, ~
 
-pub const KEY_0: c_int = 0x30; // 48, 0
-pub const KEY_1: c_int = 0x31; // 49, 1
-pub const KEY_2: c_int = 0x32; // 50, 2
-pub const KEY_3: c_int = 0x33; // 51, 3
-pub const KEY_4: c_int = 0x34; // 52, 4
-pub const KEY_5: c_int = 0x35; // 53, 5
-pub const KEY_6: c_int = 0x36; // 54, 6
-pub const KEY_7: c_int = 0x37; // 55, 7
-pub const KEY_8: c_int = 0x38; // 56, 8
-pub const KEY_9: c_int = 0x39; // 57, 9
-pub const KEY_MINUS: c_int = 0x2D; // 45, -
-pub const KEY_EQUAL: c_int = 0x3D; // 61, =
+pub const KEY_0: c_int                  = 0x30; // 48,  0
+pub const KEY_1: c_int                  = 0x31; // 49,  1
+pub const KEY_2: c_int                  = 0x32; // 50,  2
+pub const KEY_3: c_int                  = 0x33; // 51,  3
+pub const KEY_4: c_int                  = 0x34; // 52,  4
+pub const KEY_5: c_int                  = 0x35; // 53,  5
+pub const KEY_6: c_int                  = 0x36; // 54,  6
+pub const KEY_7: c_int                  = 0x37; // 55,  7
+pub const KEY_8: c_int                  = 0x38; // 56,  8
+pub const KEY_9: c_int                  = 0x39; // 57,  9
+pub const KEY_MINUS: c_int              = 0x2D; // 45,  -
+pub const KEY_EQUAL: c_int              = 0x3D; // 61,  =
 
-pub const KEY_EXCLAMATION: c_int = 0x21; // 33, !
-pub const KEY_AT: c_int = 0x40; // 64, @
-pub const KEY_HASH: c_int = 0x23; // 35, #
-pub const KEY_DOLLAR: c_int = 0x24; // 36, $
-pub const KEY_PERCENT: c_int = 0x25; // 37, %
-pub const KEY_UPPER: c_int = 0x5E; // 94, ^
-pub const KEY_AMPERSAND: c_int = 0x26; // 38, &
-pub const KEY_STAR: c_int = 0x2A; // 42, *
-pub const KEY_L_PARENTHESIS: c_int = 0x28; // 40, (
-pub const KEY_R_PARENTHESIS: c_int = 0x29; // 41, )
-pub const KEY_UNDERSCOPE: c_int = 0x5F; // 95, _
-pub const KEY_PLUS: c_int = 0x2B; // 43, +
-pub const KEY_L_CURLY_BRACES: c_int = 0x7B; // 123, {
-pub const KEY_R_CURLY_BRACES: c_int = 0x7D; // 125, }
-pub const KEY_SLASH: c_int = 0x2F; // 47, /
-pub const KEY_BACKSLASH: c_int = 0x5C; // 92, '\'
-pub const KEY_VERTICAL_BAR: c_int = 0x7C; // 124, |
+pub const KEY_EXCLAMATION: c_int        = 0x21; // 33,  !
+pub const KEY_AT: c_int                 = 0x40; // 64,  @
+pub const KEY_HASH: c_int               = 0x23; // 35,  #
+pub const KEY_DOLLAR: c_int             = 0x24; // 36,  $
+pub const KEY_PERCENT: c_int            = 0x25; // 37,  %
+pub const KEY_UPPER: c_int              = 0x5E; // 94,  ^
+pub const KEY_AMPERSAND: c_int          = 0x26; // 38,  &
+pub const KEY_STAR: c_int               = 0x2A; // 42,  *
+pub const KEY_L_PARENTHESIS: c_int      = 0x28; // 40,  (
+pub const KEY_R_PARENTHESIS: c_int      = 0x29; // 41,  )
+pub const KEY_UNDERSCOPE: c_int         = 0x5F; // 95,  _
+pub const KEY_PLUS: c_int               = 0x2B; // 43,  +
+pub const KEY_L_CURLY_BRACES: c_int     = 0x7B; // 123, {
+pub const KEY_R_CURLY_BRACES: c_int     = 0x7D; // 125, }
+pub const KEY_SLASH: c_int              = 0x2F; // 47,  /
+pub const KEY_BACKSLASH: c_int          = 0x5C; // 92, '\'
+pub const KEY_VERTICAL_BAR: c_int       = 0x7C; // 124, |
 
-pub const KEY_COLON: c_int = 0x3A; // 58, :
-pub const KEY_SEMICOLON: c_int = 0x3B; // 59, ;
-pub const KEY_SINGLE_QUOTE: c_int = 0x27; // 39, '
-pub const KEY_DOUBLE_QUOTE: c_int = 0x22; // 34, "
-pub const KEY_COMMA: c_int = 0x2C; // 44, ,
-pub const KEY_PERIOD: c_int = 0x2E; // 46, .
-pub const KEY_L_ARROW_BRACKET: c_int = 0x3C; // 60, <
-pub const KEY_R_ARROW_BRACKET: c_int = 0x3E; // 62, >
+pub const KEY_COLON: c_int              = 0x3A; // 58, :
+pub const KEY_SEMICOLON: c_int          = 0x3B; // 59, ;
+pub const KEY_SINGLE_QUOTE: c_int       = 0x27; // 39, '
+pub const KEY_DOUBLE_QUOTE: c_int       = 0x22; // 34, "
+pub const KEY_COMMA: c_int              = 0x2C; // 44, ,
+pub const KEY_PERIOD: c_int             = 0x2E; // 46, .
+pub const KEY_L_ARROW_BRACKET: c_int    = 0x3C; // 60, <
+pub const KEY_R_ARROW_BRACKET: c_int    = 0x3E; // 62, >
 
-pub const KEY_D: c_int = 0x64; // 100, d
-pub const KEY_E: c_int = 0x65; // 101, e
-pub const KEY_F: c_int = 0x66; // 102, f
-pub const KEY_P: c_int = 0x70; // 112, p
-pub const KEY_S: c_int = 0x73; // 115, s
+pub const KEY_D: c_int                  = 0x64; // 100, d
+pub const KEY_E: c_int                  = 0x65; // 101, e
+pub const KEY_F: c_int                  = 0x66; // 102, f
+pub const KEY_P: c_int                  = 0x70; // 112, p
+pub const KEY_S: c_int                  = 0x73; // 115, s
 
 //
 // 0x0D (13)
@@ -304,37 +296,37 @@ pub const NCURSES_ATTR_SHIFT: u32 = 8;
 //
 // Preconstined attributes
 //
-pub const A_COLOR: u32 = 65280;
-pub const A_ATTRIBUTES: u32 = 4294967040;
-pub const A_NORMAL: u32 = 0;
-pub const A_STANDOUT: u32 = 65536;
-pub const A_UNDERLINE: u32 = 131072;
-pub const A_REVERSE: u32 = 262144;
-pub const A_BLINK: u32 = 524288;
-pub const A_DIM: u32 = 1048576;
-pub const A_BOLD: u32 = 2097152;
-pub const A_ALTCHARSET: u32 = 4194304;
-pub const A_INVIS: u32 = 8388608;
-pub const A_PROTECT: u32 = 16777216;
-pub const A_HORIZONTAL: u32 = 33554432;
-pub const A_LEFT: u32 = 67108864;
-pub const A_LOW: u32 = 134217728;
-pub const A_RIGHT: u32 = 268435456;
-pub const A_TOP: u32 = 536870912;
-pub const A_VERTICAL: u32 = 1073741824;
-pub const A_ITALIC: u32 = 2147483648;
+pub const A_COLOR: u32          = 65280;
+pub const A_ATTRIBUTES: u32     = 4294967040;
+pub const A_NORMAL: u32         = 0;
+pub const A_STANDOUT: u32       = 65536;
+pub const A_UNDERLINE: u32      = 131072;
+pub const A_REVERSE: u32        = 262144;
+pub const A_BLINK: u32          = 524288;
+pub const A_DIM: u32            = 1048576;
+pub const A_BOLD: u32           = 2097152;
+pub const A_ALTCHARSET: u32     = 4194304;
+pub const A_INVIS: u32          = 8388608;
+pub const A_PROTECT: u32        = 16777216;
+pub const A_HORIZONTAL: u32     = 33554432;
+pub const A_LEFT: u32           = 67108864;
+pub const A_LOW: u32            = 134217728;
+pub const A_RIGHT: u32          = 268435456;
+pub const A_TOP: u32            = 536870912;
+pub const A_VERTICAL: u32       = 1073741824;
+pub const A_ITALIC: u32         = 2147483648;
 
 //
 // Preconstined colors
 //
-pub const COLOR_BLACK: u16 = 0;
-pub const COLOR_RED: u16 = 1;
-pub const COLOR_GREEN: u16 = 2;
-pub const COLOR_YELLOW: u16 = 3;
-pub const COLOR_BLUE: u16 = 4;
-pub const COLOR_MAGENTA: u16 = 5;
-pub const COLOR_CYAN: u16 = 6;
-pub const COLOR_WHITE: u16 = 7;
+pub const COLOR_BLACK: u16      = 0;
+pub const COLOR_RED: u16        = 1;
+pub const COLOR_GREEN: u16      = 2;
+pub const COLOR_YELLOW: u16     = 3;
+pub const COLOR_BLUE: u16       = 4;
+pub const COLOR_MAGENTA: u16    = 5;
+pub const COLOR_CYAN: u16       = 6;
+pub const COLOR_WHITE: u16      = 7;
 
 //
 // Preconstined types
@@ -357,7 +349,11 @@ pub extern fn start_color() c_int;
 //
 pub extern fn init_color(index: ColorIndex, r: Color, g: Color, b: Color) c_int;
 
-pub extern fn init_pair(index: ColorPairIndex, foreground_color_index: ColorIndex, background_color_index: ColorIndex) c_int;
+pub extern fn init_pair(
+    index: ColorPairIndex,
+    foreground_color_index: ColorIndex,
+    background_color_index: ColorIndex,
+) c_int;
 
 pub fn color_pair(index: ColorPairIndex) VideoAttr {
     return ((index << (0 + NCURSES_ATTR_SHIFT)) & A_COLOR);
@@ -385,12 +381,22 @@ pub extern const acs_map: [128]chtype;
 
 //
 // `chtype` means a unsigned character, e.g.:
-// 	'a'~'z', 'A'~ Z', '0'~'9'
-// 	'*', '?', '>', '<', '=', etc.
+//     'a'~'z', 'A'~ Z', '0'~'9'
+//     '*', '?', '>', '<', '=', etc.
 //
-// 	'0' means use the constault charater to fill the border!!!
+//     '0' means use the constault charater to fill the border!!!
 //
-pub extern fn wborder(win: ?*WINDOW, left: chtype, right: chtype, top: chtype, bottom: chtype, top_left_corner: chtype, top_right_corner: chtype, bottom_left_corner: chtype, bottom_right_corner: chtype) c_int;
+pub extern fn wborder(
+    win: ?*WINDOW,
+    left: chtype,
+    right: chtype,
+    top: chtype,
+    bottom: chtype,
+    top_left_corner: chtype,
+    top_right_corner: chtype,
+    bottom_left_corner: chtype,
+    bottom_right_corner: chtype
+) c_int;
 
 pub fn box(win: ?*WINDOW, left_right: chtype, top_bottom: chtype) c_int {
     return wborder(
@@ -755,7 +761,7 @@ pub fn create_popup_window(left: c_int, top: c_int, width: c_int, height: c_int,
 //     // //
 //     // const item_len = items.len;
 //     // for( let index=0z; index < item_len; index +=1) {
-//     // 	self.items[index] = items[index];
+//     //     self.items[index] = items[index];
 //     // };
 //
 //     self.win = newwin(self.height, self.width, self.top, self.left);
